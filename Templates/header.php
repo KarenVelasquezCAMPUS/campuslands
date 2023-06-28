3ca5e9d16b099e4ffb9ab3e6d414404cbb02431c
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2023-06-28 08:40:20 -0500

    feat: :tada: composer e importaciones

---
+++ Templates/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+	<link rel="stylesheet" href="css/style.css">
+	<script src="main.js" async></script>
+	<script src="app.js" defer></script>
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+    <script src="js/bootstrap/bootstrap.min.js" defer></script>
+	<title> campuslands </title>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+    include_once __DIR__.'/Templates/header.php';
+?>
+
+    <h1> Hello World </h1>
+
+<?php
+    include_once __DIR__. '/Templates/footer.php';
+?>
